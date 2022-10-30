5e1fb4a45fafe17aef6e6e7fc933fa1ba69e9c96
Olexandr Sherbakov <107261823+Olexandr5@users.noreply.github.com>
2022-10-30 11:07:37 +0200

    0.2
    0.2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+// console.log(galleryItems);
+const divGallery = document.querySelector('.gallery');
+const creatGallleryMarkap = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}" 
+      data-source=="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+divGallery.innerHTML = creatGallleryMarkap;
+divGallery.addEventListener('click', divGalleryOn);
+function divGalleryOn(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image'));
+  {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `img scr = "${event.target.dataset.sourse}" widt="800" heigth="600"`,
+    {
+      onShow: instance => {
+        window.addEventListener('keydown', closeModal);
+      },
+      onClose: instance => {
+        window.removeEventListener('keydown', closeModal);
+      },
+    }
+  );
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
